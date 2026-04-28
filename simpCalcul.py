212ed6abd265a0a2030d8c3d7339f9520ad64272
Katsumi <danilch2108gmail.com>
2026-04-28 12:18:53 +0200

    added simpCalcul.py

---
+++ simpCalcul.py
@@ -0,0 +1,32 @@
+def operations() :
+    import operator
+    import math
+    ops = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+    }
+    expression = input()
+    for op in ops :
+        if op in expression :
+            a, b = expression.split(op)
+            result = ops[op](float(a), float(b))
+            print("result: ", result, "\n")
+            break
+    else :
+        print("unvalid command")
+while True :
+    print("This is a calculator")
+    print("If you'l want to Exit press 0, or every key to continue:")
+    Exit = input("?:")
+    if Exit == "0" :
+        print("Goodbye")
+        break
+    else :
+        print("Put your operation: ")
+    calculator = operations()
+
+
+
+
